dc9270a92cb7a368194e08bc2011ba1736a47724
EgorBo <egorbo@gmail.com>
2024-09-01 19:38:03 +0200

    t

--- Program.cs
+++ Program.cs
@@ -81,8 +81,8 @@ internal class Program
                 {
                     try
                     {
-                        reply += $"\n\n🔥 Flame graphs (Click to expand), `Before`: ![Main]({await UploadFileToAzure(azToken, azContainer, baseFlame, id)})\n vs `After`:\n";
-                        reply += $"![PR]({await UploadFileToAzure(azToken, azContainer, diffFlame, id)})\n";
+                        reply += $"\n\n🔥 Flame graphs: [Main]({await UploadFileToAzure(azToken, azContainer, baseFlame, id)})\n vs ";
+                        reply += $"[PR]({await UploadFileToAzure(azToken, azContainer, diffFlame, id)})\n";
                         reply += $"\nHot asm: [Main]({await CreateGistAsync(gtApp, ghToken, $"base_asm_{id}.asm", ReadContentSafe(baseHotAsm))}) vs ";
                         reply += $"[PR]({await CreateGistAsync(gtApp, ghToken, $"diff_asm_{id}.asm", ReadContentSafe(diffHotAsm))})\n";
                         reply += $"Hot functions: [Main]({await CreateGistAsync(gtApp, ghToken, $"base_functions_{id}.txt", ReadContentSafe(baseHotFuncs))}) vs ";
@@ -98,7 +98,7 @@ internal class Program
                 {
                     try
                     {
-                        reply += $"\n\n🔥 Flame graphs (Click to expand): ![Main]({await UploadFileToAzure(azToken, azContainer, baseFlame, id)})\n\n";
+                        reply += $"\n\n🔥 Flame graphs: [Main]({await UploadFileToAzure(azToken, azContainer, baseFlame, id)})\n";
                         reply += $"Hot asm: [Main]({await CreateGistAsync(gtApp, ghToken, $"base_asm_{id}.asm", ReadContentSafe(baseHotAsm))})\n";
                         reply += $"Hot functions: [Main]({await CreateGistAsync(gtApp, ghToken, $"base_functions_{id}.txt", ReadContentSafe(baseHotFuncs))})\n";
                         reply += "\n_For clean `perf` results, make sure you have just one `[Benchmark]` in your app._\n";
